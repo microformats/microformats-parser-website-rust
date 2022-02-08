9934fb3b54b61d436a00722f4ac197b8f6efb5de
Jacky Alciné <yo@jacky.wtf>
2022-02-08 15:53:06 -0500

    Agh, figuring out the static compiled vs compiled is hard.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use serde_json::json;
 use trillium::Conn;
 use trillium::Status;
 use trillium_router::Router;
-use trillium_static::StaticFileHandler;
+use trillium_static_compiled::include_dir;
+use trillium_static_compiled::StaticCompiledHandler;
 use trillium_tera::TeraConnExt;
 use trillium_tera::{Tera, TeraHandler};
 use url::Url;
@@ -84,7 +85,7 @@ async fn web() {
                     conn
                 }
             }),
-            StaticFileHandler::new("static").with_index_file("index.html"),
+            StaticCompiledHandler::new(include_dir!("./static")).with_index_file("index.html"),
         ))
         .await
 }
